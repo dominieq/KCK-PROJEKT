504fb11a7612af7a9277c5a221eebbeb4cc65de2
vonshick <kubawasik@vp.pl>
2018-11-12 18:56:07 +0100

    Code cleaning

--- adjuster.py
+++ adjuster.py
@@ -13,65 +13,52 @@ import argparse
 def order_points(pts):
 	# sort the points based on their x-coordinates
 	xSorted = pts[np.argsort(pts[:, 0]), :]
- 
-	# grab the left-most and right-most points from the sorted
-	# x-roodinate points
+
+	#choose left and right points separately 
 	leftMost = xSorted[:2, :]
 	rightMost = xSorted[2:, :]
- 
-	# now, sort the left-most coordinates according to their
-	# y-coordinates so we can grab the top-left and bottom-left
-	# points, respectively
+	
+	#sort by y-value : the one with smaller y will be the top left corner
 	leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
 	(tl, bl) = leftMost
- 
-	# now that we have the top-left coordinate, use it as an
-	# anchor to calculate the Euclidean distance between the
-	# top-left and right-most points; by the Pythagorean
-	# theorem, the point with the largest distance will be
-	# our bottom-right point
-	D = dist.cdist(tl[np.newaxis], rightMost, "euclidean")[0]
-	(br, tr) = rightMost[np.argsort(D)[::-1], :]
- 
-	# return the coordinates in top-left, top-right,
-	# bottom-right, and bottom-left order
+	
+	rightMost = rightMost[np.argsort(rightMost[:, 1]), :]
+	(tr, br) = rightMost
+
+	# #we can count the distance between top left corner and right corners
+	# #bigger distance will show us the buttom right corner 
+	# D = dist.cdist(tl[np.newaxis], rightMost, "euclidean")[0]
+	# (br, tr) = rightMost[np.argsort(D)[::-1], :]
+
 	return np.array([tl, tr, br, bl], dtype="float32")
 
-def four_point_transform(image, pts):
+def extract_sheet(image, pts):
 	# obtain a consistent order of the points and unpack them
 	# individually
 	rect = order_points(pts)
 	(tl, tr, br, bl) = rect
  
-	# compute the width of the new image, which will be the
-	# maximum distance between bottom-right and bottom-left
-	# x-coordiates or the top-right and top-left x-coordinates
+	#count the top and bottom widht (Pythagorean theorem)
 	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
 	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
 	maxWidth = max(int(widthA), int(widthB))
- 
-	# compute the height of the new image, which will be the
-	# maximum distance between the top-right and bottom-right
-	# y-coordinates or the top-left and bottom-left y-coordinates
+
+	#count the hights 
 	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
 	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
 	maxHeight = max(int(heightA), int(heightB))
  
-	# now that we have the dimensions of the new image, construct
-	# the set of destination points to obtain a "birds eye view",
-	# (i.e. top-down view) of the image, again specifying points
-	# in the top-left, top-right, bottom-right, and bottom-left
-	# order
+	#specify new rectangle containing part of image
 	dst = np.array([
 		[0, 0],
 		[maxWidth - 1, 0],
 		[maxWidth - 1, maxHeight - 1],
 		[0, maxHeight - 1]], dtype = "float32")
  
-	# compute the perspective transform matrix and then apply it
+	#count the perspective transform matrix and then apply it
 	M = cv2.getPerspectiveTransform(rect, dst)
 	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-	# return the warped image
+
 	return warped
 
 def main():
@@ -85,7 +72,8 @@ def main():
 	# increase accuracy of edge detection and speed up image processing
 	# image = cv2.imread(args["image"])
 	for i in [4,5,6,8,9,10,13,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]:
-		if i < 19:
+		print(i)
+		if i < 10:
 			file = "Resources/nutki_0"+repr(i)+".JPG"
 		else:
 			file = "Resources/nutki_"+repr(i)+".JPG"
@@ -135,7 +123,7 @@ def main():
 		# cv2.destroyAllWindows()
 
 		#turn the sheet of paper using original image
-		warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+		warped = extract_sheet(orig, screenCnt.reshape(4, 2) * ratio)
 		
 		# convert the warped image to grayscale, then threshold it
 		# to give it that 'black and white' paper effect
