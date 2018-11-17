9ccf3866819320b2a5b25188e5fc16ea26de81e8
vonshick <kubawasik@vp.pl>
2018-11-17 19:39:53 +0100

    blob_detector.py refactor

--- blob_detector.py
+++ blob_detector.py
@@ -9,6 +9,52 @@ def display_image(title, img):
 		cv2.waitKey(0) & 0xFF
 		cv2.destroyAllWindows()
 
+def enumerate_notes(im_with_blobs, keypoints, staffs):
+    staff_diff = 3 / 5 * (staffs[0].max_range - staffs[0].min_range)
+    # lista dwuelementowych list zawierających [min-diff, max+diff] - poszerzamy pięciolinię
+    bins = [x for sublist in [[staff.min_range - staff_diff, staff.max_range + staff_diff] for staff in staffs] for x in
+            sublist]
+
+    keypoints_staff = np.digitize([key.pt[1] for key in keypoints], bins)
+    sorted_notes = sorted(list(zip(keypoints, keypoints_staff)), key=lambda tup: (tup[1], tup[0].pt[0]))
+
+    im_with_numbers = im_with_blobs.copy()
+
+    for idx, tup in enumerate(sorted_notes):
+        cv2.putText(im_with_numbers, str(idx), (int(tup[0].pt[0]), int(tup[0].pt[1])),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1, color=(255, 0, 0))
+        cv2.putText(im_with_blobs, str(tup[1]), (int(tup[0].pt[0]), int(tup[0].pt[1])),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1, color=(255, 0, 0))
+    return im_with_numbers, im_with_blobs, sorted_notes
+
+def find_blobs(image):
+    # Set up the SimpleBlobDetector with default parameters.
+    params = cv2.SimpleBlobDetector_Params()
+    params.minThreshold = 10
+    params.maxThreshold = 3000
+    params.minDistBetweenBlobs = 0
+    params.filterByArea = True
+    params.minArea = 250
+    params.maxArea = 700
+##     whole notes params
+#     params.minArea = 250
+#     params.maxArea = 410
+    params.filterByCircularity = False
+#     params.minCircularity = 0
+    params.filterByConvexity = False
+#     params.minConvexity = 0
+    params.filterByInertia = False
+#     params.minInertiaRatio = 0.01
+
+    detector = cv2.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(image)
+
+    cv2.drawKeypoints(image, keypoints=keypoints, outImage=image, color=(0, 0, 255),
+                      flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    return image, keypoints
+
 def find_contours(original, thresholded):
     contours = cv2.findContours(thresholded, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if imutils.is_cv2() else contours[1] #OpenCV 2.4 and OpenCV 3 return contours differently
@@ -21,9 +67,8 @@ def find_contours(original, thresholded):
     background = cv2.cvtColor(original, cv2.COLOR_GRAY2BGR)    
     with_notes = cv2.drawContours(background, contours_notes, -1, (0,0,255), 2)
     with_notes = imutils.resize(with_notes, height = 1000)
-#     display_image("siema",with_notes)
-    cv2.imwrite("blobs/")
-    return
+    # display_image("siema",with_notes)
+    return with_notes
 
 def remove_lines(im_with_blobs, method, size = 11, off = 10):
     T = threshold_local(im_with_blobs, size, offset = off, method = method)#generic, mean, median
@@ -42,76 +87,23 @@ def detect_blobs(input_image, staffs):
     """
     im_with_blobs = input_image.copy()
 
-    horizontal_removed = remove_lines(im_with_blobs, "mean", size = 11, off = 20)
-#     good version for blob detection below
-#     horizontal_removed = cv2.erode(horizontal_removed, np.ones((9, 5)))    
+    horizontal_removed = remove_lines(im_with_blobs, "mean", size = 11, off = 20) 
     horizontal_removed_contours = cv2.erode(horizontal_removed, np.ones((7, 5)))    
 
-    find_contours(im_with_blobs, horizontal_removed_contours)
+    with_contours = find_contours(im_with_blobs, horizontal_removed_contours)
 
 #   better version of cv2.erode parameters for blob detection
     horizontal_removed = cv2.erode(horizontal_removed, np.ones((9, 5)))
     
-    # im_with_blobs = vertical_lines
     im_with_blobs = horizontal_removed
     im_with_blobs = cv2.cvtColor(im_with_blobs, cv2.COLOR_GRAY2BGR)
 
-    # Set up the SimpleBlobDetector with default parameters.
-    params = cv2.SimpleBlobDetector_Params()
-    params.minThreshold = 10
-    params.maxThreshold = 3000
-    params.minDistBetweenBlobs = 0
-    params.filterByArea = True
-    params.minArea = 250
-    params.maxArea = 700
-#whole notes params
-#     params.minArea = 250
-#     params.maxArea = 410
-    params.filterByCircularity = False
-#     params.minCircularity = 0
-    params.filterByConvexity = False
-#     params.minConvexity = 0
-    params.filterByInertia = False
-#     params.minInertiaRatio = 0.01
+    im_with_blobs, keypoints = find_blobs(im_with_blobs)
 
-    detector = cv2.SimpleBlobDetector_create(params)
-#     detector = cv2.SimpleBlobDetector_create()
-    keypoints = detector.detect(im_with_blobs)
-
-    cv2.drawKeypoints(im_with_blobs, keypoints=keypoints, outImage=im_with_blobs, color=(0, 0, 255),
-                      flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    # cv2.imwrite("Witam.jpg", im_with_blobs)
-
-    # return horizontal_removed, im_with_blobs
-
-    # '''
-    # Here we enumerate notes.
-    # '''
-    staff_diff = 3 / 5 * (staffs[0].max_range - staffs[0].min_range)
-    # lista dwuelementowych list zawierających [min-diff, max+diff] - poszerzamy pięciolinię
-    #
-    bins = [x for sublist in [[staff.min_range - staff_diff, staff.max_range + staff_diff] for staff in staffs] for x in
-            sublist]
-
-    keypoints_staff = np.digitize([key.pt[1] for key in keypoints], bins)
-    sorted_notes = sorted(list(zip(keypoints, keypoints_staff)), key=lambda tup: (tup[1], tup[0].pt[0]))
-
-    im_with_numbers = im_with_blobs.copy()
-
-    for idx, tup in enumerate(sorted_notes):
-        cv2.putText(im_with_numbers, str(idx), (int(tup[0].pt[0]), int(tup[0].pt[1])),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=1, color=(255, 0, 0))
-        cv2.putText(im_with_blobs, str(tup[1]), (int(tup[0].pt[0]), int(tup[0].pt[1])),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=1, color=(255, 0, 0))
-
-    cv2.imwrite("output/8c_with_numbers.jpg", im_with_numbers)
-    cv2.imwrite("output/8d_with_staff_numbers.jpg", im_with_blobs)
+    im_with_numbers, im_with_blobs, sorted_notes = enumerate_notes(im_with_blobs, keypoints, staffs)
 
     print("Keypoints length : " + str(len(keypoints)))
 
-    return horizontal_removed, im_with_blobs
+    return horizontal_removed, im_with_blobs, with_contours
 
     # return sorted_notes
